4230ac12025ca1685de33a1ca4d17edcf154d08a
Sean Mull <kimlan065@gmail.com>
2021-07-21 22:49:15 -0700

    Added saved files

--- hashTable/PrintAnagramsTogether.java
+++ hashTable/PrintAnagramsTogether.java
@@ -1,6 +1,33 @@
 package hashTable;
 
 public class PrintAnagramsTogether {
+	/*Given an array of strings, return all groups of strings that are anagrams. The groups must be created in order of their appearance in the original array. Look at the sample case for clarification.
+
+
+			Example 1:
+
+			Input:
+			N = 5
+			words[] = {act,god,cat,dog,tac}
+			Output: 
+			god dog
+			act cat tac
+			Explanation:
+			There are 2 groups of
+			anagrams "god", "dog" make group 1.
+			"act", "cat", "tac" make group 2.
+			Example 2:
+
+			Input:
+			N = 3
+			words[] = {no,on,is}
+			Output: 
+			no on
+			is
+			Explanation:
+			There are 2 groups of
+			anagrams "no", "on" make group 1.
+			"is" makes group 2.*/
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- hashTable/SubArraysWithSumK.java
+++ hashTable/SubArraysWithSumK.java
@@ -1,6 +1,30 @@
 package hashTable;
 
 public class SubArraysWithSumK {
+	/*Given an unsorted array of integers, find the number of continuous subarrays having sum exactly equal to a given number k.
+
+
+	Example 1:
+
+	Input:
+	N = 5
+	Arr = {10 , 2, -2, -20, 10}
+	k = -10
+	Output: 3
+	Explaination: 
+	Subarrays: arr[0...3], arr[1...4], arr[3..4]
+	have sum exactly equal to -10.
+
+	Example 2:
+
+	Input:
+	N = 6
+	Arr = {9, 4, 20, 3, 10, 5}
+	k = 33
+	Output: 2
+	Explaination: 
+	Subarrays : arr[0...2], arr[2...4] have sum
+	exactly equal to 33.*/
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
