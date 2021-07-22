28985d9ab92bb3e0b4986a4587dc3730d4d5deeb
Sean Mull <kimlan065@gmail.com>
2021-07-21 22:36:42 -0700

    Commit KfreqElement

--- hashTable/KFreqElement.java
+++ hashTable/KFreqElement.java
@@ -1,5 +1,11 @@
 package hashTable;
 
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+
 public class KFreqElement {
 	/*Given a non-empty array of integers, find the top k elements which have the highest frequency in the array. 
 	 * If two numbers have the same frequency then the larger number should be given preference. 
@@ -22,8 +28,28 @@ public class KFreqElement {
 	as 2 > 1.*/
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] nums = {1,1,1,2,2,3};
+		HashMap<Integer,Integer> map = getFreq(nums);
+		int maxFreq = 0;
+		
+		for(int v : map.values()) maxFreq = Math.max(maxFreq, v);
+		
+		int[] n = new int[maxFreq + 1];
+		
+		for(int k : map.keySet()) n[map.get(k)] = k;
+		
+		for(int i = (n.length -1); i > 1; i--) System.out.println(n[i]);
+	}
+	
+	public static HashMap<Integer,Integer> getFreq (int[] nums){
+		HashMap<Integer,Integer> m = new HashMap<Integer,Integer>();
+		for(int num: nums) {
+			int tmp = 1;
+			if(m.containsKey(num)) tmp = m.get(num) + 1;
+			m.put(num, tmp);
+		}
+		return m;
+		
 	}
 
 }
