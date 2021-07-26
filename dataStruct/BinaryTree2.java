bffc1d1e8ab781359d7f97f6995302f17e25bc31
Sean Mull <kimlan065@gmail.com>
2021-07-25 22:29:03 -0700

    Commits binaryTree

--- dataStruct/BinaryTree2.java
+++ dataStruct/BinaryTree2.java
@@ -1,93 +1,101 @@
 package dataStruct;
 
 public class BinaryTree2 {
-	Node root = null;		
-	
+	Node root = null;
 	class Node{
-		int value;
-		Node leftChild = null;
-		Node rightChild = null;
-		Node(int value){
-			this.value = value;
+		int val;
+		Node left;
+		Node right;
+		Node(int val){
+			this.val = val;
+			this.left = this.right = null;
 		}
 	}
 	
-	public void inorderTraversal(Node n) {
-		if(n == null) return;
-		inorderTraversal(n.leftChild);
-		visit(n);
-		inorderTraversal(n.rightChild);
-	}
-	
-	public void visit(Node n) {
-		System.out.println(n.value);
-	}
-	
-	public boolean binaryTreeSearch(Node n, int val) {
-		if(n == null) return false;
-		
-		if(n.value == val) {
-			return true;
-		}else if(n.value > val) {
-			return binaryTreeSearch(n.leftChild, val);
-		}else {
-			return binaryTreeSearch(n.rightChild, val);
-		}
-	}
-	
-	public boolean add(Node n, int val) {
+	public Node add(Node curNode, int val) {
 		if(root == null) {
 			root = new Node(val);
-			return true;
+			return curNode;
 		}
-		if(n.value == val) {
-			return false;
-		}else if(n.value > val) {
-			if(n.leftChild == null) {
-				n.leftChild = new Node(val);
-				return true;
+		
+		if(curNode.val > val) {
+			if(curNode.left == null) {
+				curNode.left = new Node(val);
 			}else {
-				return add(n.leftChild, val);
+				return add(curNode.left, val);
 			}
 		}else {
-			if(n.rightChild == null) {
-				n.rightChild = new Node(val);
-				return true;
+			if(curNode.right == null) {
+				curNode.right = new Node(val);
 			}else {
-				return add(n.rightChild, val);
+				return add(curNode.right, val);
 			}
+		}
+		return curNode;
+	}
+	
+	public boolean search(Node curNode,int val) {
+		if(curNode == null) return false;
+		if(curNode.val == val) return true;
+
+		if(curNode.val > val) {
+			return search(curNode.left, val);
+		}else{
+			return search(curNode.right, val);
 		}
 	}
 	
-	public boolean remove(Node n, Node parent, int val) {
-		if (n == null) return false;
+	public boolean remove(Node curNode, Node parent, int val) {
+		if(curNode == null) return false;
 		
-		if (val < n.value) {
-			return remove(n.leftChild, n, val);
-		}else if(val > n.value) {
-			return remove(n.rightChild, n, val);
-		}else {
-			if(n.leftChild != null && n.rightChild != null) {
-				//n.value = maxValue(n.leftChild);  //create max value where we get the max of left subtree
-				remove(n.leftChild, n, n.value);
-			}else if(parent.leftChild == n) {
-				parent.leftChild = n.leftChild != null ? n.leftChild : n.rightChild;
+		if(curNode.val > val) {
+			return remove(curNode.left, curNode, val);
+		}else if(curNode.val < val){
+			return remove(curNode.right, curNode, val);
+		}else{
+			if(curNode.left != null && curNode.right != null) {
+				curNode.val = findMax(curNode.left);
+				remove(curNode.left, curNode, curNode.val);
+			}else if(parent.left == curNode) {
+				parent.left = (curNode.left != null) ? curNode.left : curNode.right;
 			}else {
-				parent.rightChild = n.leftChild != null? n.leftChild : n.rightChild;
+				parent.right = (curNode.right != null) ? curNode.left : curNode.right;
 			}
 			return true;
 		}
+	}
+	
+	public int findMax(Node curNode) {
+		if(curNode.right == null) {
+			return curNode.val;
+		}else {
+			return findMax(curNode.right);
+		}
+		
+		
+	}
+	
+	public void inOrderTrav (Node n) {
+		if(n != null) {
+			inOrderTrav(n.left);
+			printNode(n);
+			inOrderTrav(n.right);
+		}
+	}
+	
+	public void printNode(Node n) {
+		System.out.println(n.val);
 	}
 
 	public static void main(String[] args) {
-		int[] arr = {20,4,23,1,2,45,12,89};
-		BinaryTree2 tree = new BinaryTree2();
-		for(int ele: arr) {
-			tree.add(tree.root, ele);
-		}
-		tree.inorderTraversal(tree.root);
+		BinaryTree2 Tree = new BinaryTree2();
+		int[] arr = {3,4,2,5,1,7,0};
+		for(int ele: arr) Tree.add(Tree.root, ele);
+		//Tree.inOrderTrav(Tree.root);
+		//System.out.println(Tree.search(Tree.root,22));
+		//System.out.println(Tree.findMax(Tree.root.left));
+		Tree.remove(Tree.root, Tree.root, 7);
+		Tree.inOrderTrav(Tree.root);
 	}
-	
-	
 
 }
